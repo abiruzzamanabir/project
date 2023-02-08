847817dc03d0129f8e03b610e6fb57839781cb6f
abiruzzamanabir <abiruzzamanabir17.com>
2023-02-08 12:16:19 +0600

    queue add

--- app/Notifications/Notification/PasswordChangeSuccessfullNotification.php
+++ app/Notifications/Notification/PasswordChangeSuccessfullNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class PasswordChangeSuccessfullNotification extends Notification
+class PasswordChangeSuccessfullNotification extends Notification implements ShouldQueue
 {
     use Queueable;
     private $password;
